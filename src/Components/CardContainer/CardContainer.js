60112a3e75c01368cdecabb0e93bf9fe66870cbd
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-02 20:47:52 -0600

    Add promise array WIP.

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -3,9 +3,18 @@ import css from './CardContainer.css'
 import Card from '../Card/Card'
 
 const CardContainer = ( { peopleData }) => {
+  let names;
+  if (peopleData.results) {
+
+    names = peopleData.results.map((person, index) => {
+      return <Card key={index} name={person.name} />
+    })
+    console.log(peopleData.results[0]);
+  }
+
   return (
-    <div>
-      <Card peopleData={peopleData}/>
+    <div className="card-container">
+      {names}
     </div>
   )
 }
