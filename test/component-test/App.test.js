e9af745ce99910440bad52a3f4a48ff98ed67d06
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-07 22:25:08 -0600

    Add initial App testing

--- test/component-test/App.test.js
+++ test/component-test/App.test.js
@@ -1,4 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '../../src/Components/Button/Button';
+import App from '../../src/Components/App/App';
+import Scroller from '../../src/Components/Scroller/Scroller';
 import { shallow, mount } from 'enzyme';
+import fetchMock from 'fetch-mock'
+import 'whatwg-fetch';
+
+it('should mount', () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper.find('.app-body').length).toBe(1);
+})
+
+it('Mounts all the child components', () => {
+  const wrapper = mount(<App />);
+  console.log(wrapper.debug());
+
+  expect(wrapper.find('.scroller').length).toBe(1);
+  expect(wrapper.find('.header-container').length).toBe(1);
+  expect(wrapper.find('.button-container').length).toBe(1);
+  expect(wrapper.find('.card-container').length).toBe(1);
+})
+
+it('should fetch the scroller', () => {
+
+})
