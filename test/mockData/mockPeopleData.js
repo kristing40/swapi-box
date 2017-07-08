9c21a957851b590352bc3f12621be8afaa1ac42b
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-08 17:44:04 -0600

    Add get vehicles data test WIP.

---
+++ test/mockData/mockPeopleData.js
@@ -0,0 +1,41 @@
+export const mockPeopleData = {
+    "count": 87,
+    "next": "http://swapi.co/api/people/?page=2",
+    "previous": null,
+    "results": [
+        {
+            "name": "Luke Skywalker",
+            "homeworld": "http://swapi.co/api/planets/1/",
+            "species": [
+                "http://swapi.co/api/species/1/"
+            ],
+        },
+        {
+            "name": "C-3PO",
+            "homeworld": "http://swapi.co/api/planets/1/",
+            "species": [
+                "http://swapi.co/api/species/2/"
+            ],
+        },
+    ]
+}
+
+
+
+export const mockHomeWorldLuke = {
+    "name": "Tatooine"
+}
+
+export const mockHomeWorldC3PO = {
+  "name": "Tatooine"
+}
+
+export const mockSpeciesLuke = {
+  "name": "Human",
+  "language": "Galactic Basic"
+}
+
+export const mockSpeciesC3PO = {
+  "name": "Droid",
+  "language": "n/a"
+}
---
+++ test/mockData/mockVehicleData.js
@@ -0,0 +1,17 @@
+export const mockVehicleData = {
+    "results": [
+        {
+            "name": "Sand Crawler",
+            "model": "Digger Crawler",
+            "passengers": "30",
+            "vehicle_class": "wheeled",
+        },
+        {
+            "name": "T-16 skyhopper",
+            "model": "T-16 skyhopper",
+            "manufacturer": "Incom Corporation",
+            "passengers": "1",
+            "vehicle_class": "repulsorcraft",
+        },
+    ]
+}
